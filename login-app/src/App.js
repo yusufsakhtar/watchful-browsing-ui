94cc92836a3da804e5c538e816a6970da542e7d0
Ahmed  Hussain <ahmedhussain@Ahmed-Hussains-MacBook-Pro.local>
2024-10-12 22:11:35 -0500

    Login Screen

---
+++ login-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginScreen from './LoginScreen.js'
+
+function App() {
+  return (
+    <div className="App">
+      <LoginScreen /> {LoginScreen}
+    </div>
+  );
+}
+
+export default App;
